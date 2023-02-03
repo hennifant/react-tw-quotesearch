08a3ef29ad79e7e9163aed477714e356dfe023e4
hennifant <henning.schwichow@gmx.de>
2023-02-03 12:14:57 +0100

    Initializing imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [tag, setTag] = useState("love");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="container mx-auto text-center pt-20">
+        <h1 className="font-bold text-gray-800 text-5xl">
+          Get Inspired by Quotes
+        </h1>
+      </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center justify-center text-center mt-5"
+      >
+        <input
+          type="text"
+          name="tag"
+          placeholder="search quote.."
+          className="border-2 border-gray-800 px-2 py-2"
+          value={tag}
+          onChange={(e) => setTag(e.target.value)}
+        />
+        <button
+          type="submit"
+          className=" bg-gray-800 text-white px-4 py-2 border-2 border-gray-800 hover:bg-transparent hover:text-gray-800"
         >
-          Learn React
-        </a>
-      </header>
+          Search
+        </button>
+      </form>
     </div>
   );
 }
